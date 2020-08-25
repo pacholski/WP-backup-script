ac7296db71acab133ccfc11c3bd47f8c481fd1f8
NPROFIT <kontakt.pacholski@gmail.com>
2020-08-25 15:24:13 +0200

    Added links to download files

--- backup.php
+++ backup.php
@@ -5,28 +5,27 @@ $DB_USER = DB_USER;
 $DB_PASSWORD = DB_PASSWORD;
 $DB_NAME = DB_NAME;
 
-// Names of generated files
-$BCKP_DB_NAME = "database-bckp".".gz";
-$BCKP_FILES_NAME = "files-bckp".".zip";
-
-function getDatabaseZipped($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME) {
-        $command = "mysqldump --opt -h '".$DB_HOST."' -u '".$DB_USER."' -p'".$DB_PASSWORD."' '".$DB_NAME."' | gzip > ".$BCKP_DB_NAME."";
+    function getDatabaseZipped($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME) {
+        $command = "mysqldump --opt -h '".$DB_HOST."' -u '".$DB_USER."' -p'".$DB_PASSWORD."' '".$DB_NAME."' | gzip > database.gz";
         exec($command);
     }
 
-function getFilesZipped() {
-        exec("zip -r ".$BCKP_FILES_NAME." './'");
-        echo "Remember to delete ".$BCKP_FILES_NAME." after downloading! ";
+    function getFilesZipped() {
+        shell_exec("zip -r files.zip './'");
     }
 
 getFilesZipped();
 getDatabaseZipped($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME);
 
+echo "<a href='files.zip'>Download Files</a><br>";
+echo "<a href='database.gz'>Download Database</a><br><br>";
+        echo "<b style='color:red;'>Remember to delete files after downloading! </b><br>";
+
 // Delete backup script from host.
 $toDelete = "backup.php";
 if (!unlink($toDelete)) {  
-    echo (" $toDelete can't be deleted. ");  
+    echo ("$toDelete can't be deleted.");  
 }  
 else {  
-    echo (" $toDelete deleted. ");  
+    echo ("$toDelete deleted.");  
 } 
